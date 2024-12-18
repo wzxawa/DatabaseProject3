dfb3a3f1fe732d15273e1c8fd7dc7ddc3bb271d2
Wzxawa <Wzxawa@gmail.com>
2024-12-19 00:00:31 +0800

    configure and hammerdbtest1

---
+++ Configure/show.sql
@@ -0,0 +1,10 @@
+--SHOW.sql
+--数据库内存相关参数
+SHOW shared_buffers;
+SHOW work_mem;
+SHOW maintenance_work_mem;
+SHOW effective_cache_size;
+--网络和连接相关参数
+SHOW listen_addresses;
+SHOW port;
+SHOW max_connections;
